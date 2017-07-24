2c3f8043313cc0f71574d42393ffd2403adc149f
baoen_nie <baoen_nie@ssic.cn>
2017-07-24 12:00:30 +0800

    基本框架搭建

---
+++ sendlog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SendlogConfig(AppConfig):
+    name = 'sendlog'
---
+++ sendmo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SendmoConfig(AppConfig):
+    name = 'sendmo'
